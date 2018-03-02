a7340290f2cf8fc00d46de24a58e9bccae4fe9d1
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-03-02 10:46:00 -0800

    add shoe and store add forms with basic css and jquery

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
-require("bundler/setup")
-require("pry")
+require 'bundler/setup'
+require 'pry'
 
 Bundler.require(:default)
 
---
+++ public/js/scripts.js
@@ -0,0 +1,21 @@
+$(function() {
+  $('#show_add_store').click(function() {
+    $('#add_store').slideDown();
+    $('#show_add_store').hide();
+  })
+
+  $('#show_add_shoe').click(function() {
+    $('#add_shoe').slideDown();
+    $('#show_add_shoe').hide();
+  })
+
+  $('#hide_add_store').click(function() {
+    $('#add_store').hide();
+    $('#show_add_store').show();
+  })
+
+  $('#hide_add_shoe').click(function() {
+    $('#add_shoe').hide();
+    $('#show_add_shoe').show();
+  })
+})
